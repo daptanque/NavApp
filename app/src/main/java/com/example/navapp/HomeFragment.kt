7af891f5ba0463997ee5f6f87a5c6fe0444dd6ee
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-17 14:22:18 +0000

    Setting up navigation and btn set OnClick event

--- app/src/main/java/com/example/navapp/HomeFragment.kt
+++ app/src/main/java/com/example/navapp/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.navapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,8 +19,12 @@ class HomeFragment : Fragment() {
     ): View? {
 
         // Inflate the layout for this fragment
-
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        //handle button clickevent
+        binding.btnSubmit.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
 
         return binding.root
 
